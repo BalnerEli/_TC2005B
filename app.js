53b6b036b505abd342d3da767b704a6f575f6aff
BalnerEli <A01703315@tec.mx>
2023-08-20 21:04:08 -0600

    Lab 4 completado

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const filesystem = require('fs');
 filesystem.writeFileSync("hola.txt","Hola desde node")
 
 console.log("ya acabe de escribir");
+
+const http = required ("http");
+
+const server = http.createServer ((request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type','text/html');
+    response.write("Hoooola");
+    response,end();
+});
+
+server.listen(3000);
